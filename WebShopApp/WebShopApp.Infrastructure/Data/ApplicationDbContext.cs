ea5d3c3e6b53ef859c850f2e4055bf5984f3d0cd
izabellaT <izabellatodorova84@gmail.com>
2023-12-07 11:32:56 +0200

    ene

--- WebShopApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
+++ WebShopApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -5,15 +5,14 @@ using WebShopApp.Infrastructure.Data.Domain;
 
 namespace WebShopApp.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
             this.Database.EnsureCreated();
-        }
-        public DbSet<Brand> Brands { get; set; }
-        public DbSet<Category> categories { get; set; }
+        }        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
     }
